1fac9f6517ee20966eed7fae4bf771be3cd9e8b6
SylvieShare <wrz2@mail.ru>
2026-08-18 20:08:21 +0300

    Обновить обложки благословения и ауры живучести

--- internal/itemcovers/catalog.go
+++ internal/itemcovers/catalog.go
@@ -21,8 +21,8 @@ var assets embed.FS
 
 var Catalog = []Image{
 	{Key: "fireball", FileName: "fireball.webp", ObjectKey: "system-item-covers/v1/spells/fireball.webp", Size: 89752, SHA256: "d0a950a6ae7301505c5700e806b27e215e8d0dc3bf143ddac3d9d28f6df7c1a5", MimeType: "image/webp", TypeID: 5, Aliases: []string{"fireball", "огненныйшар"}},
-	{Key: "bless", FileName: "bless.webp", ObjectKey: "system-item-covers/v1/spells/bless.webp", Size: 59630, SHA256: "ef7f798700b308ff47e1e4ac6c5354aa351500bacb4264bd27fa9cde79df5e13", MimeType: "image/webp", TypeID: 5, Aliases: []string{"bless", "blessing", "благословение"}},
-	{Key: "aura-of-vitality", FileName: "aura-of-vitality.webp", ObjectKey: "system-item-covers/v1/spells/aura-of-vitality.webp", Size: 84690, SHA256: "4ea6ec8df4cba89f88079d54a658c6aafcd12100d2149a33116265b3ccde927b", MimeType: "image/webp", TypeID: 5, Aliases: []string{"auraofvitality", "аураживучести"}},
+	{Key: "bless", FileName: "bless.webp", ObjectKey: "system-item-covers/v1/spells/bless.webp", Size: 45714, SHA256: "c07aa8bbc2c9600d1b2d5dc2c4b60559997609f85923439ef4ea728f5c131c12", MimeType: "image/webp", TypeID: 5, Aliases: []string{"bless", "blessing", "благословение"}},
+	{Key: "aura-of-vitality", FileName: "aura-of-vitality.webp", ObjectKey: "system-item-covers/v1/spells/aura-of-vitality.webp", Size: 52452, SHA256: "b920859cc1cfad5a1e229facd8bef4417d69d546a236382e8a55278821da3cb7", MimeType: "image/webp", TypeID: 5, Aliases: []string{"auraofvitality", "аураживучести"}},
 }
 
 func Read(image Image) ([]byte, error) {
